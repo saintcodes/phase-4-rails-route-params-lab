db9f89c6e288e9713258128b16268e860e9441f3
Michelle <clairsin@gmail.com>
2022-06-28 13:07:55 -0400

    commit tree 4f50947ff00a259b38f8b032bb3c076352806fd3

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,17 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
+    students = if params[:name]
+      Student.by_name(params[:name])
+    else
+      Student.all
+    end    
     render json: students
+  end
+
+  def show_by_id
+    show = Student.find_by(id: params[:id])
+    render json: show
   end
 
 end
